b6b97807462b87a9cb64f8b4193fe2d4f992cf32
jkim68888 <jkim68888@gmail.com>
2022-01-13 12:51:25 +0900

    홈의 헤더 컴포넌트 완성

--- components/home/Header.js
+++ components/home/Header.js
@@ -15,6 +15,9 @@ const Header = () => (
         <Image style={styles.icon} source={require('../../assets/heart.png')} />
       </TouchableOpacity>
       <TouchableOpacity>
+        <View style={styles.unreadBadge}>
+          <Text style={styles.unreadBadgeText}>11</Text>
+        </View>
         <Message width="30" height="30" />
       </TouchableOpacity>
     </View>
@@ -41,6 +44,22 @@ const styles = StyleSheet.create({
     height: 30,
     marginRight: 7,
     resizeMode: 'contain',
+  },
+  unreadBadge: {
+    backgroundColor: '#ff3250',
+    position: 'absolute',
+    left: 12,
+    bottom: 20,
+    width: 25,
+    height: 18,
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: 100,
+  },
+  unreadBadgeText: {
+    color: 'white',
+    fontWeight: '600',
   },
 })
 
